86c55b6ac6a126ffd7632a8125d5c2a7befe8f9f
João Paulo <62178699+joaop2p@users.noreply.github.com>
2024-12-30 16:52:57 -0300

    Update database.py
    remove comments

--- src/app/services/database.py
+++ src/app/services/database.py
@@ -4,7 +4,6 @@ from sqlmodel import Field, SQLModel, create_engine, Session, select
 class Conect:
     def __init__(self):
         super().__init__()
-        # Usar pymysql como driver de conexão
         self.engine = create_engine("mysql+pymysql://root:@127.0.0.1:3306/sistema_storage")
         SQLModel.metadata.create_all(self.engine)
 
@@ -24,6 +23,4 @@ class Users(SQLModel, table=True):
     # TODO criar uma tabela de log com os eventos de acesso à aplicação
     # TODO criar um código token para validação
 
-# Criar uma instância da classe Conect para testar a conexão
-# result: Users = Conect().select(Users, id=1, login="jpxns")
-# print(result)
+
